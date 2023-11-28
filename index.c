5c053491d64f8f57b0cdcd90d8a4d3e1622a0883
Adithya Sailesh <97180744+Adithya-Sailesh@users.noreply.github.com>
2023-11-29 00:18:15 +0530

    Update index.c

--- index.c
+++ index.c
@@ -1,70 +1,46 @@
 #include<stdio.h>
-#include<stdlib.h>
-struct node{
-    int data;
-    struct node *link;
-}*first,*last,*newnode,*temp;
-int start,num;
-struct node mem[50];
-int isallocated(){
-    for(int i=start;i<=start+num;i++){
-        if(mem[i].data==1){
-            printf("Cannot Allocate");
-            return 1;
-        }
-    }
-    return 0;
-}
-void linked(){
-    printf("Enter the starting block");
-    scanf("%d",&start);
-    printf("Enter the number of block");
-    scanf("%d",&num);
-    int flag=isallocated();
-    if(flag==0){
-        for(int i=start;i<start+num;i++){
-            int n;
-            printf("Enter the block number");
-            scanf("%d",&n);
-            mem[i].data=1;
-            newnode=(struct node *)malloc(sizeof(struct node ));
-            newnode->data=n;
-            newnode->link=NULL;
-            if(first==NULL){
-                first=newnode;
-                last=newnode;
-            }
-            else{
-                last->link=newnode;
-                last=newnode;
-            }
-        }
-        printf("Linked List Is");
-        temp=first;
-        first=last=NULL;
-        for(int i=start;i<start+num;i++){
-            printf("%d ->",temp->data);
-            temp=temp->link;
-        }
-    }
-    else{
-        printf("Already Allocated");
-    }
-    
-}
 void main(){
-    printf("----------LINKED ALLOCATION------------");
-    while (1)
-    {   int ch;
-        printf("Enter 1 for new new file \n 2 for exit");
+    int files[50],index[50]={0};
+    printf("----------INDEXED FILE----------\n");
+    while(1){
+        int ind,n,ch,flag=0;
+        printf("\nEnter the choice \n1) to add file \n2)exit\n");
         scanf("%d",&ch);
         if(ch==1){
-            linked();
+            printf("Enter the index block");
+            scanf("%d",&ind);
+            if(files[ind]==1){
+                printf("Index Already in Use");
+            }
+            else{
+                files[ind]=1;
+                printf("\nEnter the number of blocks nedded");
+                scanf("%d",&n);
+                printf("\nEnter Block Numbers");
+                for(int i=0;i<n;i++){
+                    scanf("%d",&index[i]);
+                    if(files[index[i]]==1){
+                        flag=1;
+                    }
+                    else{
+                        files[index[i]]=1;
+                    }
+                }
+                if(flag==1){
+                    printf("Block Already allocated");
+                    files[ind]=0;
+                }
+                else{
+                    printf("\n----FILE ALLOCATED----\n");
+                    for (int i=0; i<n; i++){
+                        printf("%d ------> %d\n", ind, index[i],files[index[i]]);
+                    }  
+                } 
+            }
         }
         else{
+            printf("Program Exited !!!!!!!!!!!!!!!");
             break;
         }
     }
-    
-    
 }
